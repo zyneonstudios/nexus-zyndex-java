01448cc9d03096c2291711b73fc85f9a7042d432
nerotvlive <daniel.niesmann@outlook.com>
2024-04-20 18:38:45 +0200

    Remove "Main method"

--- src/main/java/com/zyneonstudios/nexus/Main.java
+++ src/main/java/com/zyneonstudios/nexus/Main.java
@@ -1,19 +1,16 @@
 package com.zyneonstudios.nexus;
 
-import com.zyneonstudios.nexus.index.ReadableZyndex;
-import com.zyneonstudios.nexus.instance.ReadableZynstance;
 import live.nerotv.shademebaby.logger.Logger;
 
 public class Main {
 
     public static Logger logger = new Logger("ZDX");
-    public static String[] args = null;
 
     public static void main(String[] a) {
-        args = a;
-        ReadableZyndex index = new ReadableZyndex("https://raw.githubusercontent.com/zyneonstudios/nexus-nex/main/zyndex/index.json");
-        for(ReadableZynstance instance:index.getInstances()) {
-            System.out.println(instance.getName());
-        }
+        logger.log("=======================================================");
+        logger.log("This is the Zyndex API by Zyneon Nexus");
+        logger.log("https://nexus.zyneonstudios.com");
+        logger.log("https://github.com/zyneonstudios/nexus-zyndex-java");
+        logger.log("=======================================================");
     }
 }
